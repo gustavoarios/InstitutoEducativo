3b52888ac8e4bd432cf8b4f15998a18ee089a082
gustavoarios <gustavoadonairios@gmail.com>
2025-04-09 16:15:32 -0300

    Creacion de clases Alumno y Carrera

--- Instituto.C/Instituto.C/Models/Alumno.cs
+++ Instituto.C/Instituto.C/Models/Alumno.cs
@@ -1,6 +1,41 @@
 ﻿namespace Instituto.C.Models
 {
-    public class Alumno
+    public class Alumno : Persona
     {
+        //declaro las propiedades del Alumno
+        public int Id { get; set; }
+        public int NumeroMatricula { get; set; }
+        public Carrera Carrera { get; set; }
+        public List<Inscripcion> Inscripciones { get; set; }
+        public List<Calificacion> Calificaciones { get; set; }
+
+        //constructor por defecto
+        public Alumno() : base()
+        {
+            Inscripciones = new List<Inscripcion>();
+            Calificaciones = new List<Calificacion>();
+        }
+
+        //constructor parametrizado
+        public Alumno(int idPersona, string userName, string email, string nombre, string apellido, string dni, string telefono, string direccion, int idAlumno, int numeroMatricula, Carrera carrera) : base()
+        {
+            base.Id = idPersona;
+            UserName = userName;
+            Email = email;
+            Nombre = nombre;
+            Apellido = apellido;
+            DNI = dni;
+            Telefono = telefono;
+            Direccion = direccion;
+            Activo = true;
+            FechaAlta = DateTime.Now;
+
+            this.Id = idAlumno;
+            Carrera = carrera;
+            NumeroMatricula = numeroMatricula;
+            Inscripciones = new List<Inscripcion>();
+            Calificaciones = new List<Calificacion>();
+
+        }
     }
 }
--- Instituto.C/Instituto.C/Models/Carrera.cs
+++ Instituto.C/Instituto.C/Models/Carrera.cs
@@ -2,5 +2,26 @@
 {
     public class Carrera
     {
+        //propiedades de la Carrera
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public List<Materia> Materias { get; set; }
+        public List<Alumno> Alumnos { get; set; }
+
+        //constructor por defecto
+        public Carrera()
+        {
+            Materias = new List<Materia>();
+            Alumnos = new List<Alumno>();
+        }
+
+        //constructor parametrizado
+        public Carrera(String nombre, int id)
+        {
+            Nombre = nombre;
+            Id = id;
+            Materias = new List<Materia>();
+            Alumnos = new List<Alumno>();
+        }
     }
 }
