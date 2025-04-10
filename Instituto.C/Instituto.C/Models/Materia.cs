73c837d2c58d468aad20c79ca99fc1f847c21455
paomattjie <paolamattjie@hotmail.com>
2025-04-10 11:07:18 -0300

    Propiedades para clases Materia, MateriaCursada

--- Instituto.C/Instituto.C/Models/Materia.cs
+++ Instituto.C/Instituto.C/Models/Materia.cs
@@ -2,5 +2,13 @@
 {
     public class Materia
     {
+        //propiedades de la Materia
+        public int Id { get; set; } //Id de la materia
+        public Carrera Carrera { get; set; } //Nombre de la Carrera
+        public string Nombre { get; set; } //Ej: "Programacion I"
+        public string CodigoMateria { get; set; } //Ej: "P1"
+        public string Descripcion { get; set; } //Texto libre
+        public int CupoMaximo { get; set; } //Capacidad maxima de alumnos por comision
+        public List<MateriaCursada> Cursadas { get; set; }
     }
 }
