1e2d3a6e9ee28801b9292c2edcb6025e6163ab9f
gustavoarios <gustavoadonairios@gmail.com>
2025-04-10 12:02:57 -0300

    Propiedades para clases Alumno, Carrera

--- Instituto.C/Instituto.C/Models/Alumno.cs
+++ Instituto.C/Instituto.C/Models/Alumno.cs
@@ -9,33 +9,5 @@
         public List<Inscripcion> Inscripciones { get; set; }
         public List<Calificacion> Calificaciones { get; set; }
 
-        //constructor por defecto
-        public Alumno() : base()
-        {
-            Inscripciones = new List<Inscripcion>();
-            Calificaciones = new List<Calificacion>();
-        }
-
-        //constructor parametrizado
-        public Alumno(int idPersona, string userName, string email, string nombre, string apellido, string dni, string telefono, string direccion, int idAlumno, int numeroMatricula, Carrera carrera) : base()
-        {
-            base.Id = idPersona;
-            UserName = userName;
-            Email = email;
-            Nombre = nombre;
-            Apellido = apellido;
-            DNI = dni;
-            Telefono = telefono;
-            Direccion = direccion;
-            Activo = true;
-            FechaAlta = DateTime.Now;
-
-            this.Id = idAlumno;
-            Carrera = carrera;
-            NumeroMatricula = numeroMatricula;
-            Inscripciones = new List<Inscripcion>();
-            Calificaciones = new List<Calificacion>();
-
-        }
     }
 }
--- Instituto.C/Instituto.C/Models/Carrera.cs
+++ Instituto.C/Instituto.C/Models/Carrera.cs
@@ -8,20 +8,5 @@
         public List<Materia> Materias { get; set; }
         public List<Alumno> Alumnos { get; set; }
 
-        //constructor por defecto
-        public Carrera()
-        {
-            Materias = new List<Materia>();
-            Alumnos = new List<Alumno>();
-        }
-
-        //constructor parametrizado
-        public Carrera(String nombre, int id)
-        {
-            Nombre = nombre;
-            Id = id;
-            Materias = new List<Materia>();
-            Alumnos = new List<Alumno>();
-        }
     }
 }
