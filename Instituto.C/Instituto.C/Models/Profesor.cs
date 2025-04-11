43ac9f2adcdf807efba8ce0d84b6b67d1edcb33a
BenjaminCesar3 <cesarbmontalti@gmail.com>
2025-04-11 14:24:34 -0300

    Propiedades para clases Persona, Empleado, Profesor

--- Instituto.C/Instituto.C/Models/Profesor.cs
+++ Instituto.C/Instituto.C/Models/Profesor.cs
@@ -1,17 +1,7 @@
 ﻿namespace Instituto.C.Models
 {
-    public class Profesor
+    public class Profesor : Persona
     {
-        public int Id { get; set; }
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public DateTime FechaAlta { get; set; }
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
-        public string DNI { get; set; }
-        public string Telefono { get; set; }
-        public string Direccion { get; set; }
-        public bool Activo { get; set; }
         public string Legajo { get; set; }
         public List<MateriaCursada> MateriasCursada { get; set; }
         public List<Calificacion> Calificaciones { get; set; }
