a3bd0c570c95f36e02581905de7b9a7629da2532
ruizidona <ruiz.nicolasign@gmaill.com>
2025-04-11 13:51:43 -0700

    Revert "Propiedades para clases Inscripcion, Calificacion, Nota"
    This reverts commit 3b6f28a0fd4ce2271ee2d72aa09b4657b1e350f3.

--- Instituto.C/Instituto.C/Models/Calificacion.cs
+++ Instituto.C/Instituto.C/Models/Calificacion.cs
@@ -2,11 +2,5 @@
 {
     public class Calificacion
     {
-        public int Id { get; set; }
-        public DateTime Fecha { get; set; }
-        public Nota Nota { get; set; }
-        public Profesor Profesor { get; set; }
-        public Inscripcion Inscripcion { get; set; }
-        public Alumno Alumno { get; set; }
     }
 }
--- Instituto.C/Instituto.C/Models/Inscripcion.cs
+++ Instituto.C/Instituto.C/Models/Inscripcion.cs
@@ -2,10 +2,5 @@
 {
     public class Inscripcion
     {
-        public int Id { get; set; }
-        public Alumno Alumno { get; set; }
-        public MateriaCursada MateriaCursada { get; set; }
-        public DateTime FechaInscripcion { get; set; }
-        public bool Activo { get; set; }
     }
 }
