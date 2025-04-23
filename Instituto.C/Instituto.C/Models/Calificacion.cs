970c12db5c9c420c451b8f2c35665b2609026684
ruizidona <ruiz.nicolasign@gmaill.com>
2025-04-22 18:13:12 -0700

    Correciones Issue 4

--- Instituto.C/Instituto.C/Models/Calificacion.cs
+++ Instituto.C/Instituto.C/Models/Calificacion.cs
@@ -1,13 +1,33 @@
-﻿namespace Instituto.C.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Instituto.C.Models
 {
     public class Calificacion
     {
+        
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar la fecha de calificación")]
         public DateTime Fecha { get; set; }
+
+        [Required(ErrorMessage = "Debe seleccionar una nota")]
         public Nota Nota { get; set; }
+
+
+        [Required(ErrorMessage = "El ID del profesor es obligatorio")]
+        public int ProfesorId { get; set; }
         public Profesor Profesor { get; set; }
+
+        [Required(ErrorMessage = "El ID del alumno es obligatorio")]
+        public int AlumnoId { get; set; }
+
+        [Required(ErrorMessage = "El ID de la materia cursada es obligatorio")]
+        public int MateriaCursadaId { get; set; }
+
+
         public Inscripcion Inscripcion { get; set; }
-        public Alumno Alumno { get; set; }
+
 
     }
 }
--- Instituto.C/Instituto.C/Models/Inscripcion.cs
+++ Instituto.C/Instituto.C/Models/Inscripcion.cs
@@ -1,12 +1,23 @@
-﻿namespace Instituto.C.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Instituto.C.Models
 {
     public class Inscripcion
     {
-        public int Id { get; set; }
+        // Propiedades relacionales
+        public int AlumnoId { get; set; }
+        public int MateriaCursadaId { get; set; }
+
+        // Propiedades Navegacionales
         public Alumno Alumno { get; set; }
         public MateriaCursada MateriaCursada { get; set; }
+
+        [Required(ErrorMessage = "Debe indicar la fecha de inscripción")]
+        [DataType(DataType.Date)]
         public DateTime FechaInscripcion { get; set; }
-        public bool Activo { get; set; }
+        [Required(ErrorMessage = "El estado de la inscripción es obligatorio")]
+        public bool Activa { get; set; }
 
     }
 }
