d0369c1915c31848401cd8d167039e47be5803ff
ruizidona <ruiz.nicolasign@gmaill.com>
2025-04-26 09:36:57 -0700

    Configuraciones base del EP2

--- Instituto.C/Instituto.C/Models/Carrera.cs
+++ Instituto.C/Instituto.C/Models/Carrera.cs
@@ -1,10 +1,24 @@
-﻿namespace Instituto.C.Models
+﻿using System.Collections.Generic;
+using Instituto.C.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace Instituto.C.Models
 {
     public class Carrera
     {
         //propiedades de la Carrera
         public int Id { get; set; }
+
+        [Required(ErrorMessage = Messages.CampoObligatorio)]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = Messages.StrMaxMin)]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = Messages.RestriccionLetras)]
         public string Nombre { get; set; }
+
+        [Required(ErrorMessage = Messages.CampoObligatorio)]
+        [StringLength(10, MinimumLength = 2, ErrorMessage = Messages.StrMaxMin)]
+        [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = Messages.RestriccionAlfanumerica)]
+        public string CodigoCarrera { get; set; }
+
         public List<Materia> Materias { get; set; }
         public List<Alumno> Alumnos { get; set; }
 
