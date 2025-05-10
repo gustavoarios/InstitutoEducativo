d00833dda68a50f7d5dcece599dd06d2756e3113
ruizidona <ruiz.nicolasign@gmaill.com>
2025-05-10 14:34:34 -0700

    Correccion Clases EP2

--- Instituto.C/Instituto.C/Models/Calificacion.cs
+++ Instituto.C/Instituto.C/Models/Calificacion.cs
@@ -10,7 +10,7 @@ namespace Instituto.C.Models
 
         [Required(ErrorMessage = Messages.CampoObligatorio)]
         [DataType(DataType.Date, ErrorMessage = Messages.RestriccionNumeros)]
-        public DateTime Fecha { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
 
         [Required(ErrorMessage = Messages.CampoObligatorio)]
         public Nota Nota { get; set; }
@@ -24,9 +24,11 @@ namespace Instituto.C.Models
         [Range(1, int.MaxValue, ErrorMessage = Messages.RestriccionNumeros)]
         public int AlumnoId { get; set; }
 
+        public Alumno Alumno { get; set; }
+
         [Required(ErrorMessage = Messages.CampoObligatorio)]
         [Range(1, int.MaxValue, ErrorMessage = Messages.RestriccionNumeros)]
-        public int MateriaCursadaId { get; set; }
+        public int InscripcionId { get; set; }
 
         public Inscripcion Inscripcion { get; set; }
     }
--- Instituto.C/Instituto.C/Models/Nota.cs
+++ Instituto.C/Instituto.C/Models/Nota.cs
@@ -2,11 +2,15 @@
 {
     public enum Nota
     {
-        SinNota,
-        Desaprobado,
-        Regular,
-        Aprobado,
-        MuyBueno,
-        Excelente
+        Uno = 1,
+        Dos = 2,
+        Tres = 3,
+        Cuatro = 4,
+        Cinco = 5,
+        Seis = 6,
+        Siete = 7,
+        Ocho = 8,
+        Nueve = 9,
+        Diez = 10
     }
 }
