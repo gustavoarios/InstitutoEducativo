c6f7411ce5e4ce8fbee17d2899f79d01ec369f84
BenjaminCesar3 <cesarbmontalti@gmail.com>
2025-05-27 19:16:23 -0300

    Resolucion de conflicto (1)

--- Instituto.C/Instituto.C/Models/Empleado.cs
+++ Instituto.C/Instituto.C/Models/Empleado.cs
@@ -2,7 +2,7 @@
 
 namespace Instituto.C.Models
 {
-    public abstract class Empleado : Persona
+    public class Empleado : Persona
     {
         public int _contadorLegajo = 0;
         public int Legajo { get { return GetLegajo(); } set { } }
