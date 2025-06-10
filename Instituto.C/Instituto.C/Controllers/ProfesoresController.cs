1a512b458aa842d6171e8fc53b5ec3bfa6a6e29d
BenjaminCesar3 <cesarbmontalti@gmail.com>
2025-06-10 16:43:47 -0300

    Implementando Legajo automatico a profesor y agregando columna legajo a la tabla profesores

--- Instituto.C/Instituto.C/Controllers/ProfesoresController.cs
+++ Instituto.C/Instituto.C/Controllers/ProfesoresController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Instituto.C.Data;
 using Instituto.C.Models;
+using Instituto.C.Helpers;
 
 namespace Instituto.C.Controllers
 {
@@ -19,13 +20,11 @@ namespace Instituto.C.Controllers
             _context = context;
         }
 
-        // GET: Profesores
         public async Task<IActionResult> Index()
         {
             return View(await _context.Profesores.ToListAsync());
         }
 
-        // GET: Profesores/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -43,29 +42,25 @@ namespace Instituto.C.Controllers
             return View(profesor);
         }
 
-        // GET: Profesores/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Profesores/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Legajo,Id,UserName,Email,FechaAlta,Nombre,Apellido,DNI,Telefono,Direccion,Activo")] Profesor profesor)
         {
             if (ModelState.IsValid)
             {
+                profesor.Legajo = GeneradorDeLegajo.GenerarLegajoParaProfesor(profesor);
                 _context.Add(profesor);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(profesor);
         }
-
-        // GET: Profesores/Edit/5
+        
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -81,9 +76,6 @@ namespace Instituto.C.Controllers
             return View(profesor);
         }
 
-        // POST: Profesores/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Legajo,Id,UserName,Email,FechaAlta,Nombre,Apellido,DNI,Telefono,Direccion,Activo")] Profesor profesor)
@@ -116,7 +108,6 @@ namespace Instituto.C.Controllers
             return View(profesor);
         }
 
-        // GET: Profesores/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -134,7 +125,6 @@ namespace Instituto.C.Controllers
             return View(profesor);
         }
 
-        // POST: Profesores/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
---
+++ Instituto.C/Instituto.C/Helpers/GeneradorDeLegajo.cs
@@ -0,0 +1,14 @@
+﻿using Instituto.C.Models;
+using System;
+
+namespace Instituto.C.Helpers
+{
+    public class GeneradorDeLegajo
+    {
+        public static string GenerarLegajoParaProfesor(Profesor profesor)
+        {
+            // Formato: PROF-2025-12345678 (usando DNI)
+            return $"PROF-{DateTime.Now.Year}-{profesor.DNI}";
+        }
+    }
+}
