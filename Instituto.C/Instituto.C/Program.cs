acc961a9678f3bd6ce849cc16f0f788aa89091f7
paomattjie <paolamattjie@hotmail.com>
2025-06-10 09:26:51 -0300

    SeedData - Parte 1

--- Instituto.C/Instituto.C/Program.cs
+++ Instituto.C/Instituto.C/Program.cs
@@ -12,6 +12,8 @@ namespace Instituto.C
     public class Program
     {
         public static void Main(string[] args)
+
+
         {
 
             var builder = WebApplication.CreateBuilder(args);
